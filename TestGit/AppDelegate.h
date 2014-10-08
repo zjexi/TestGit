a97fbfcb44405b7e67d0195b43aec917a0aad770
zjexi <zjexi@zjj.local>
2014-10-08 10:50:16 +0800

    Initial Commit

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by zjexi on 14-10-8.
+//  Copyright (c) 2014年 Zjexi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
